eb8985fcc417fc027f6fe884fc7b913c081711c6
Marcos Navarro <marcosnpcogollos@gmail.com>
2018-03-17 17:17:15 +0100

    Added session for login form

--- lesson2/main.py
+++ lesson2/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
-from flask import request, redirect
+from flask import request, redirect, url_for
+from flask import session
 from flask_wtf import CsrfProtect
 from modules.forms import loginForm
 
@@ -10,14 +11,26 @@ csrf = CsrfProtect(app)
 
 @app.route('/', methods = [ 'GET' ])
 def index():
+    if 'username' in session:
+        username = session['username']
+        print(username)
     return render_template( 'layout/homepage.html' )
 
 @app.route('/login', methods = [ 'GET', 'POST' ])
 def login():
-    login_form = loginForm.LoginForm()
-    if login_form.validate_on_submit():
-        return redirect('/')
+    if 'username' in session:
+        return redirect(url_for('index'))
+    login_form = loginForm.LoginForm(request.form)
+    if request.method == 'POST' and login_form.validate_on_submit():
+        session['username'] = login_form.email.data
+        return redirect(url_for('index'))
     return render_template( 'layout/login.html', form = login_form, actionForm = '/login' )
+
+@app.route('/logout')
+def logout():
+    if 'username' in session:
+        session.pop('username')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run( debug = True, port = 8000 )
